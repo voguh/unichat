e7ca923e9c1f71ed8d97bd1c44953f0ddc9064fc
Voguh <voguhofc@protonmail.com>
2024-09-25 10:11:34 -0300

    chore: remove strict mode

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -11,8 +11,4 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 
 const root = createRoot(document.querySelector('#root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />)
