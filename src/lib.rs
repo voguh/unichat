4d149a47f94ad5722d20a17f0b1f48d701a06473
Voguh <voguhofc@protonmail.com>
2024-09-29 17:11:44 -0300

    lint: fix linting of lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ async fn save_in_settings(app: tauri::AppHandle, key: &str, value: &str) -> Resu
     let pool = instances.get(DATABASE_KEY).unwrap();
 
     let exists_query = sqlx::query_as("SELECT key FROM 'settings' WHERE key = ?").bind(key);
-    let exists_result: Result<(String, ), sqlx::Error>  = exists_query.fetch_one(pool).await;
+    let exists_result: Result<(String,), sqlx::Error> = exists_query.fetch_one(pool).await;
     let exists = exists_result.map_or(false, |value| value.0 == key);
 
     let query;
@@ -55,18 +55,16 @@ fn setup(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let migrations = vec![
-        Migration {
-            version: 1,
-            description: "create_settings_table",
-            sql: "CREATE TABLE settings (key TEXT PRIMARY KEY, value TEXT)",
-            kind: MigrationKind::Up
-        }
-    ];
+    let migrations = vec![Migration {
+        version: 1,
+        description: "create_settings_table",
+        sql: "CREATE TABLE settings (key TEXT PRIMARY KEY, value TEXT)",
+        kind: MigrationKind::Up
+    }];
 
     tauri::Builder::default().setup(setup)
         .plugin(tauri_plugin_sql::Builder::default().add_migrations(DATABASE_KEY, migrations).build())
-        .invoke_handler(tauri::generate_handler![select_from_settings, save_in_settings])
+        .invoke_handler(tauri::generate_handler![select_from_settings,save_in_settings])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("Error while running UniChat application");
 }
