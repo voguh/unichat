fccae1b6a53c6c77073709489b2bad791ade6c3f
Voguh <voguhofc@protonmail.com>
2024-09-29 20:53:56 -0300

    refactor: rollback to plugin-store

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,4 @@
 use tauri::{LogicalPosition, LogicalSize, Manager, WebviewBuilder, WebviewUrl};
-use tauri_plugin_sql::{DbInstances, Migration, MigrationKind};
-
-const DATABASE_KEY: &str = "sqlite:unichat.db";
-
-#[tauri::command]
-async fn select_from_settings(app: tauri::AppHandle, key: &str) -> Result<String, String> {
-    let instances = app.state::<DbInstances>().inner().0.lock().await;
-    let pool = instances.get(DATABASE_KEY).unwrap();
-
-    let query = sqlx::query_as("SELECT * FROM 'settings' WHERE key = ?").bind(key);
-    let result: Result<(String, String), sqlx::Error> = query.fetch_one(pool).await;
-
-    result.map(|value| value.1).map_err(|e| e.to_string())
-}
-
-#[tauri::command]
-async fn save_in_settings(app: tauri::AppHandle, key: &str, value: &str) -> Result<(), String> {
-    let instances = app.state::<DbInstances>().inner().0.lock().await;
-    let pool = instances.get(DATABASE_KEY).unwrap();
-
-    let exists_query = sqlx::query_as("SELECT key FROM 'settings' WHERE key = ?").bind(key);
-    let exists_result: Result<(String,), sqlx::Error> = exists_query.fetch_one(pool).await;
-    let exists = exists_result.map_or(false, |value| value.0 == key);
-
-    let query;
-    if exists {
-        query = sqlx::query("UPDATE 'settings' value = ? WHERE key = ?").bind(value).bind(key);
-    } else {
-        query = sqlx::query("INSERT INTO 'settings'(key, value) VALUES (?, ?)").bind(key).bind(value);
-    }
-
-    query.execute(pool).await.map(|_| ()).map_err(|e| e.to_string())
-}
 
 #[tauri::command]
 async fn show_webview(app: tauri::AppHandle, label: &str) -> Result<(), String> {
@@ -70,16 +37,9 @@ fn setup(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let migrations = vec![Migration {
-        version: 1,
-        description: "create_settings_table",
-        sql: "CREATE TABLE settings (key TEXT PRIMARY KEY, value TEXT)",
-        kind: MigrationKind::Up
-    }];
-
     tauri::Builder::default().setup(setup)
-        .plugin(tauri_plugin_sql::Builder::default().add_migrations(DATABASE_KEY, migrations).build())
-        .invoke_handler(tauri::generate_handler![select_from_settings,save_in_settings,show_webview,hide_webview])
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![show_webview,hide_webview])
         .run(tauri::generate_context!())
         .expect("Error while running UniChat application");
 }
--- webapp/src/services/storageService.ts
+++ webapp/src/services/storageService.ts
@@ -1,22 +1,28 @@
-import { invoke } from '@tauri-apps/api/core'
+import { Store } from '@tauri-apps/plugin-store'
 
 export class StorageService {
+  private _store: Store
+
+  constructor() {
+    this._store = new Store('unichat.db')
+  }
+
   public async getItem<T>(key: string): Promise<T> {
     try {
-      const response = await invoke<string>('select_from_settings', { key })
+      const response = await this._store.get<string>(key)
       if (response == null) {
         return null
       }
 
       return JSON.parse(response)
-    } catch (e) {
+    } catch (_err) {
       return null
     }
   }
 
   public async setItem<T>(key: string, value: T): Promise<void> {
     try {
-      await invoke('save_in_settings', { key, value: JSON.stringify(value) })
+      await this._store.set(key, JSON.stringify(value))
     } catch (e) {
       console.log(e)
     }
