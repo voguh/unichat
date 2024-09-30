5bbea90620c3c3bb614ea8400a8171f914f0eb3a
Voguh <voguhofc@protonmail.com>
2024-09-30 10:00:21 -0300

    fix: new webviews size and url

--- src/lib.rs
+++ src/lib.rs
@@ -38,16 +38,13 @@ fn setup(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
     let window = app.get_window("main").unwrap();
     let window_size = window.inner_size().unwrap();
 
-    let pos_x = 8 + 48 + 8;
-    let pos_y = 8;
-    let width = window_size.width - (pos_x + 8);
-    let height = window_size.height - (pos_y + 8);
-    let url = WebviewUrl::External("about:blank".parse().unwrap());
-    let pos = LogicalPosition::new(pos_x, pos_y);
-    let size = LogicalSize::new(width, height);
+    let youtube_url = WebviewUrl::External("https://youtube.com".parse().unwrap());
+    let twitch_url = WebviewUrl::External("https://twitch.tv".parse().unwrap());
+    let pos = LogicalPosition::new(0, 0);
+    let size = LogicalSize::new(window_size.width - 64, window_size.height);
 
-    let youtube_chat = window.add_child(WebviewBuilder::new("youtube-chat", url.clone()), pos, size).unwrap();
-    let twitch_chat = window.add_child(WebviewBuilder::new("twitch-chat", url.clone()), pos, size).unwrap();
+    let youtube_chat = window.add_child(WebviewBuilder::new("youtube-chat", youtube_url), pos, size).unwrap();
+    let twitch_chat = window.add_child(WebviewBuilder::new("twitch-chat", twitch_url), pos, size).unwrap();
 
     youtube_chat.hide().unwrap();
     twitch_chat.hide().unwrap();
