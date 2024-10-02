bd9abf6972cd91ff62b7b46887d006cc88b1f63e
Voguh <voguhofc@protonmail.com>
2024-10-02 12:25:21 -0300

    feat: include verification to prevent multiple wapping of fetch

--- src/youtube.rs
+++ src/youtube.rs
@@ -1,26 +1,32 @@
 use serde_json::Value;
 
 pub const SCRAPPING_JS: &str = r#"
-    const originalFetch = window.fetch;
-    Object.defineProperty(window, "fetch", {
-        value: async (...args) => {
-            const res = await originalFetch(...args);
+    if (window.fetch.__WRAPPED__ !== true) {
+        const originalFetch = window.fetch;
+        Object.defineProperty(window, "fetch", {
+            value: async (...args) => {
+                const res = await originalFetch(...args);
 
-            if (res.url.startsWith("https://www.youtube.com/youtubei/v1/live_chat/get_live_chat") && res.ok) {
-                res.clone().json().then(async (parsed) => {
-                    const actions = parsed?.continuationContents?.liveChatContinuation?.actions;
+                if (res.url.startsWith("https://www.youtube.com/youtubei/v1/live_chat/get_live_chat") && res.ok) {
+                    res.clone().json().then(async (parsed) => {
+                        const actions = parsed?.continuationContents?.liveChatContinuation?.actions;
 
-                    if (actions != null && actions.length > 0) {
-                        await window.__TAURI__.core.invoke('on_message', { actions })
-                    }
-                }).catch(err => window.__TAURI__.log.error(err.message))
-            }
+                        if (actions != null && actions.length > 0) {
+                            await window.__TAURI__.core.invoke('on_message', { actions })
+                        }
+                    }).catch(err => window.__TAURI__.log.error(err.message))
+                }
 
-            return res;
-        },
-        configurable: true,
-        writable: true
-    });
+                return res;
+            },
+            configurable: true,
+            writable: true
+        });
+
+        Object.defineProperty(window.fetch, "__WRAPPED__", { value: true, configurable: true, writable: true })
+    } else {
+        window.__TAURI__.log.info("Fetch already was wrapped!")
+    }
 "#;
 
 #[tauri::command]
