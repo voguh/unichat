3b746dfd01b571a4c6ffa7737bcd0106a2f0b187
Voguh <voguhofc@protonmail.com>
2024-10-05 18:37:21 -0300

    feat: implement remove message and remove user

--- overlays/default/main.js
+++ overlays/default/main.js
@@ -20,25 +20,35 @@ function removeChildren() {
 }
 
 $(document).ready(function () {
-  const MAIN_CONTAINER = document.querySelector("#main-container")
-  const MESSAGE_TEMPLATE = document.querySelector("#chatlist_item").innerHTML
-  
-  const eventSource = new EventSource(`http://localhost:9527/events`)
-  eventSource.onmessage = function(event) {
-    const data = JSON.parse(event.data)
-    if(data.type === "unichat:message") {
-      const detail = data.detail
-      let message = MESSAGE_TEMPLATE.replaceAll("{author_id}", detail.authorId)
-      message = message.replaceAll("{message_id}", detail.messageId)
-      message = message.replaceAll("{author_color}", detail.authorDisplayColor)
-      message = message.replaceAll("{badges}", buildBadges(detail.authorBadges))
-      message = message.replaceAll("{author_display_name}", detail.authorDisplayName)
-      message = message.replaceAll("{author_color}", detail.authorDisplayColor)
-      message = message.replaceAll("{message}", detail.messageHtml)
+    const MAIN_CONTAINER = document.querySelector("#main-container")
+    const MESSAGE_TEMPLATE = document.querySelector("#chatlist_item").innerHTML
 
-      $(MAIN_CONTAINER).append(message)
-    }
+    const socket = new WebSocket('ws://localhost:9527/ws')
+    socket.addEventListener("message", function (event) {
+        const data = JSON.parse(event.data)
+        if (data.type === "unichat:message") {
+            const detail = data.detail
+            let message = MESSAGE_TEMPLATE.replaceAll("{author_id}", detail.authorId)
+            message = message.replaceAll("{message_id}", detail.messageId)
+            message = message.replaceAll("{platform}", detail.platform)
+            message = message.replaceAll("{author_color}", detail.authorDisplayColor)
+            message = message.replaceAll("{badges}", buildBadges(detail.authorBadges))
+            message = message.replaceAll("{author_display_name}", detail.authorDisplayName)
+            message = message.replaceAll("{author_color}", detail.authorDisplayColor)
+            message = message.replaceAll("{message}", detail.messageHtml)
 
-    requestAnimationFrame(removeChildren)
-  }
+            if (MAIN_CONTAINER.querySelector(`div[data-id=${data.messageId}]`) == null) {
+                $(MAIN_CONTAINER).append(message)
+            }
+        } else if (data.type === 'unichat:remove_message') {
+            MAIN_CONTAINER.querySelector(`div[data-id=${data.messageId}]`)?.remove()
+        } else if (data.type === 'unichat:remove_user') {
+            const messages = MAIN_CONTAINER.querySelectorAll(`div[data-from=${data.authorId}]`)
+            for (const message of (messages ?? [])) {
+                message.remove()
+            }
+        }
+
+        requestAnimationFrame(removeChildren)
+    })
 })
