385393b68dac7bd47c70b04b636bbfc4310784f8
Voguh <voguhofc@protonmail.com>
2024-10-08 10:57:50 -0300

    refactor: add expects in actix handler

--- src/actix/mod.rs
+++ src/actix/mod.rs
@@ -15,7 +15,9 @@ pub fn register_actix<R: tauri::Runtime>(app: &tauri::App<R>, overlays_dir: Path
             actix_web::App::new().wrap(actix_web::middleware::Logger::default())
                 .service(routes::ws)
                 .service(actix_files::Files::new("/overlays", &overlays_dir).prefer_utf8(true).index_file("index.html"))
-        }).bind(("127.0.0.1", 9527)).unwrap().run().await.unwrap()
+        })
+        .bind(("0.0.0.0", 9527)).expect("An error occurred on bind address 0.0.0.0:9527")
+        .run().await.expect("An error occurred on run actix server")
     });
 
     let state = app.state::<ActixState>();
