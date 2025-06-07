7db55ac77ddd127feb45a62e4ca11ac75f091366
Voguh <voguhofc@protonmail.com>
2025-06-07 16:58:59 -0300

    refactor: change default font to roboto

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,14 +1,18 @@
 import "normalize.css";
 import "react-toastify/dist/ReactToastify.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-import "@fontsource/nunito-sans/300"; // Regular
-import "@fontsource/nunito-sans/300-italic"; // Regular
-import "@fontsource/nunito-sans/400"; // Regular
-import "@fontsource/nunito-sans/400-italic"; // Regular
-import "@fontsource/nunito-sans/600"; // Semi-bold
-import "@fontsource/nunito-sans/600-italic"; // Semi-bold
-import "@fontsource/nunito-sans/700"; // Bold
-import "@fontsource/nunito-sans/700-italic"; // Bold
+import "@fontsource/roboto/400";
+import "@fontsource/roboto/400-italic";
+import "@fontsource/roboto/600";
+import "@fontsource/roboto/600-italic";
+import "@fontsource/roboto/700";
+import "@fontsource/roboto/700-italic";
+import "@fontsource/roboto-mono/400";
+import "@fontsource/roboto-mono/400-italic";
+import "@fontsource/roboto-mono/600";
+import "@fontsource/roboto-mono/600-italic";
+import "@fontsource/roboto-mono/700";
+import "@fontsource/roboto-mono/700-italic";
 
 import React from "react";
 import { createRoot } from "react-dom/client";
