a8bbf78e997c23ff5d7f95fb4fd83ee278ec952c
Voguh <voguhofc@protonmail.com>
2025-06-24 11:21:39 -0300

    chore: add license headers

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -1,3 +1,20 @@
+/*!******************************************************************************
+ * UniChat
+ * Copyright (C) 2025 Voguh <voguhofc@protonmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 3 only, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 use std::sync::OnceLock;
 
 use tokio::sync::broadcast;
