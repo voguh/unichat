cce26e5257135c3c6451691b29a45cf12713719e
Voguh <voguhofc@protonmail.com>
2025-06-27 19:35:36 -0300

    chore: reorder payload parser main

--- src/youtube/mapper/mod.rs
+++ src/youtube/mapper/mod.rs
@@ -24,15 +24,15 @@ mod remove_chat_item_by_author_action;
 pub mod structs;
 
 pub fn parse(payload: &serde_json::Value) -> Result<Option<UniChatEvent>, Box<dyn std::error::Error>> {
-    if let Some(value) = payload.get("addChatItemAction") {
+    if let Some(value) = payload.get("addBannerToLiveChatCommand") {
+        return add_banner_to_live_chat_command::parse(value);
+    } else if let Some(value) = payload.get("addChatItemAction") {
         return add_chat_item_action::parse(value);
     } else if let Some(value) = payload.get("removeChatItemAction") {
         return remove_chat_item_action::parse(value.clone());
     } else if let Some(value) = payload.get("removeChatItemByAuthorAction") {
         return remove_chat_item_by_author_action::parse(value.clone());
-    } else if let Some(value) = payload.get("addBannerToLiveChatCommand") {
-        return add_banner_to_live_chat_command::parse(value);
-    } else {
-        return Ok(None);
     }
+
+    return Ok(None);
 }
