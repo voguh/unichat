1261ade67030cb8ecd4b189f6736a025d4ac187e
Voguh <voguhofc@protonmail.com>
2025-06-27 23:18:41 -0300

    feat: implement sponsor and raid events

--- widgets/default/script.js
+++ widgets/default/script.js
@@ -1,6 +1,8 @@
 const MAIN_CONTAINER = document.querySelector("#main-container");
 const MESSAGE_TEMPLATE = document.querySelector("#chatlist_item").innerHTML;
 const DONATE_TEMPLATE = document.querySelector("#donate_item").innerHTML;
+const SPONSOR_TEMPLATE = document.querySelector("#sponsor_item").innerHTML;
+const RAID_ITEM = document.querySelector("#raid_item").innerHTML;
 
 function buildBadges(badges) {
   let badgeJoin = ''
@@ -63,6 +65,29 @@ window.addEventListener("unichat:event", function ({ detail: event }) {
         if (MAIN_CONTAINER.querySelector(`div[data-id=${data.messageId}]`) == null) {
             $(MAIN_CONTAINER).append(message);
         }
+    } else if (event.type === "unichat:donate") {
+        const data = event.data
+        let message = SPONSOR_TEMPLATE.replaceAll("{author_id}", data.authorId);
+        message = message.replaceAll("{platform}", data.platform);
+        message = message.replaceAll("{message_id}", data.messageId);
+        message = message.replaceAll("{author_color}", data.authorDisplayColor);
+        message = message.replaceAll("{badges}", buildBadges(data.authorBadges));
+        message = message.replaceAll("{author_display_name}", data.authorDisplayName);
+        message = message.replaceAll("{tier}", data.tier ?? "membership");
+        message = message.replaceAll("{value}", data.months);
+        message = message.replaceAll("{message}", buildMessage(data.messageText, data.emotes));
+
+        if (MAIN_CONTAINER.querySelector(`div[data-id=${data.messageId}]`) == null) {
+            $(MAIN_CONTAINER).append(message);
+        }
+    } else if (event.type === "unichat:raid") {
+        const data = event.data
+        let message = RAID_ITEM.replaceAll("{author_id}", data.authorId);
+        message = message.replaceAll("{platform}", data.platform);
+        message = message.replaceAll("{message_id}", data.messageId);
+        message = message.replaceAll("{message}", `${data.authorDisplayName} is raiding with ${data.viewers ?? 'their'} viewers!`);
+
+        $(MAIN_CONTAINER).append(message);
     } else if (event.type === 'unichat:remove_message') {
         MAIN_CONTAINER.querySelector(`div[data-id="${event.data.messageId}"]`)?.remove();
     } else if (event.type === 'unichat:remove_author') {
