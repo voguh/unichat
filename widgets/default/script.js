b321a2ac1cac31548e843aa3fc4153eaf2b265e7
Voguh <voguhofc@protonmail.com>
2025-06-29 11:34:49 -0300

    refactor(default widget): events styles

--- widgets/default/script.js
+++ widgets/default/script.js
@@ -1,9 +1,11 @@
 let INITIAL_DATA = {};
+let FIRST_LOAD = false;
 const MAIN_CONTAINER = document.querySelector("#main-container");
 const MESSAGE_TEMPLATE = document.querySelector("#chatlist_item").innerHTML;
 const DONATE_TEMPLATE = document.querySelector("#donate_item").innerHTML;
 const SPONSOR_TEMPLATE = document.querySelector("#sponsor_item").innerHTML;
-const RAID_ITEM = document.querySelector("#raid_item").innerHTML;
+const SPONSOR_GIFT_TEMPLATE = document.querySelector("#sponsor-gift_item").innerHTML;
+const RAID_TEMPLATE = document.querySelector("#raid_item").innerHTML;
 
 function buildBadges(badges) {
   let badgeJoin = ''
@@ -16,7 +18,7 @@ function buildBadges(badges) {
 }
 
 function buildMessage(message, emotes) {
-    let safeMessage = message.replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
+    let safeMessage = (message ?? "").replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
     if (!emotes || emotes.length === 0) {
         return safeMessage;
     }
@@ -37,70 +39,89 @@ function removeChildren() {
   }
 }
 
+window.addEventListener("unichat:load", function ({ detail: data }) {
+    INITIAL_DATA = data;
+
+    if (FIRST_LOAD === false) {
+        FIRST_LOAD = true;
+        MAIN_CONTAINER.style.setProperty("--maximum-width", window.innerWidth + "px");
+    }
+});
+
 window.addEventListener("unichat:event", function ({ detail: event }) {
-    if (event.type === "unichat:init") {
-        INITIAL_DATA = event.data;
-    } else if (event.type === "unichat:message") {
+    if (event.type === "unichat:message") {
         const data = event.data
-        let message = MESSAGE_TEMPLATE.replaceAll("{author_id}", data.authorId);
-        message = message.replaceAll("{platform}", data.platform);
-        message = message.replaceAll("{message_id}", data.messageId);
-        message = message.replaceAll("{author_color}", data.authorDisplayColor);
-        message = message.replaceAll("{badges}", buildBadges(data.authorBadges));
-        message = message.replaceAll("{author_display_name}", data.authorDisplayName);
-        message = message.replaceAll("{message}", buildMessage(data.messageText, data.emotes));
+        let htmlTemplate = MESSAGE_TEMPLATE.replace("{author_id}", data.authorId);
+        htmlTemplate = htmlTemplate.replace("{platform}", data.platform);
+        htmlTemplate = htmlTemplate.replace("{message_id}", data.messageId);
+        htmlTemplate = htmlTemplate.replace("{badges}", buildBadges(data.authorBadges));
+        htmlTemplate = htmlTemplate.replace("{author_display_name}", data.authorDisplayName);
+        htmlTemplate = htmlTemplate.replace("{message}", buildMessage(data.messageText, data.emotes));
 
         if (MAIN_CONTAINER.querySelector(`div[data-id="${data.messageId}"]`) == null) {
-            $(MAIN_CONTAINER).append(message);
+            $(MAIN_CONTAINER).append(htmlTemplate);
         }
     } else if (event.type === "unichat:donate") {
         const data = event.data
-        let message = DONATE_TEMPLATE.replaceAll("{author_id}", data.authorId);
-        message = message.replaceAll("{platform}", data.platform);
-        message = message.replaceAll("{message_id}", data.messageId);
-        message = message.replaceAll("{author_color}", data.authorDisplayColor);
-        message = message.replaceAll("{badges}", buildBadges(data.authorBadges));
-        message = message.replaceAll("{author_display_name}", data.authorDisplayName);
-        message = message.replaceAll("{currency}", data.currency);
-        message = message.replaceAll("{value}", data.value);
-        message = message.replaceAll("{message}", buildMessage(data.messageText, data.emotes));
+        let htmlTemplate = DONATE_TEMPLATE.replace("{author_id}", data.authorId);
+        htmlTemplate = htmlTemplate.replace("{platform}", data.platform);
+        htmlTemplate = htmlTemplate.replace("{message_id}", data.messageId);
+        htmlTemplate = htmlTemplate.replace("{badges}", buildBadges(data.authorBadges));
+        htmlTemplate = htmlTemplate.replace("{author_display_name}", data.authorDisplayName);
+        htmlTemplate = htmlTemplate.replace("{donate_meta}", `Just tipped <span class="value">${data.currency} ${data.value}</span>!`);
+        htmlTemplate = htmlTemplate.replace("{message}", buildMessage(data.messageText, data.emotes));
 
         if (MAIN_CONTAINER.querySelector(`div[data-id="${data.messageId}"]`) == null) {
-            $(MAIN_CONTAINER).append(message);
+            $(MAIN_CONTAINER).append(htmlTemplate);
         }
     } else if (event.type === "unichat:sponsor") {
         const data = event.data
-        let message = SPONSOR_TEMPLATE.replaceAll("{author_id}", data.authorId);
-        message = message.replaceAll("{platform}", data.platform);
-        message = message.replaceAll("{message_id}", data.messageId);
-        message = message.replaceAll("{author_color}", data.authorDisplayColor);
-        message = message.replaceAll("{badges}", buildBadges(data.authorBadges));
-        message = message.replaceAll("{author_display_name}", data.authorDisplayName);
-        message = message.replaceAll("{tier}", data.tier ?? "membership");
-        message = message.replaceAll("{months}", data.months);
-        message = message.replaceAll("{message}", buildMessage(data.messageText, data.emotes));
+        let htmlTemplate = SPONSOR_TEMPLATE.replace("{author_id}", data.authorId);
+        htmlTemplate = htmlTemplate.replace("{platform}", data.platform);
+        htmlTemplate = htmlTemplate.replace("{message_id}", data.messageId);
+        htmlTemplate = htmlTemplate.replace("{badges}", buildBadges(data.authorBadges));
+        htmlTemplate = htmlTemplate.replace("{author_display_name}", data.authorDisplayName);
+        htmlTemplate = htmlTemplate.replace("{tier}", data.tier);
+        const msgBegin = data.platform === "youtube" ? "Become a member" : "Become a subscriber";
+        if (data.months != null) {
+            const message = `${msgBegin} for <span>${data.months}</span> months<br/>with tier <span>${data.tier}</span>!`;
+            htmlTemplate = htmlTemplate.replace("{sponsor_meta}", message);
+        } else {
+            const message = `${msgBegin} with tier <span>${data.tier}</span>!`;
+            htmlTemplate = htmlTemplate.replace("{sponsor_meta}", message);
+        }
+        htmlTemplate = htmlTemplate.replace("{message}", buildMessage(data.messageText, data.emotes));
 
         if (MAIN_CONTAINER.querySelector(`div[data-id="${data.messageId}"]`) == null) {
-            $(MAIN_CONTAINER).append(message);
+            $(MAIN_CONTAINER).append(htmlTemplate);
         }
     } else if (event.type === "unichat:sponsor_gift") {
         const data = event.data
-        let message = RAID_ITEM.replaceAll("{author_id}", data.authorId);
-        message = message.replaceAll("{platform}", data.platform);
-        message = message.replaceAll("{message_id}", data.messageId);
-        message = message.replaceAll("{message}", `${data.authorDisplayName}, just gifted ${data.count} ${data.platform === "youtube" ? "memberships": "subscriptions"} ${data.tier ? `with tier ${data.tier}`: ""}!`);
+        let htmlTemplate = SPONSOR_GIFT_TEMPLATE.replaceAll("{author_id}", data.authorId);
+        htmlTemplate = htmlTemplate.replaceAll("{platform}", data.platform);
+        htmlTemplate = htmlTemplate.replaceAll("{message_id}", data.messageId);
+        let message = `<span>${data.authorDisplayName}</span>, just gifted `;
+        if (data.count > 1) {
+            message += `<span>${data.count} ${data.platform === "youtube" ? "memberships": "subscriptions"}</span>`;
+        } else {
+            message += `<span>${data.count} ${data.platform === "youtube" ? "membership": "subscription"}</span>`;
+        }
+        if (data.tier) {
+            message += `with tier ${data.tier}`;
+        }
+        htmlTemplate = htmlTemplate.replaceAll("{message}", `${message}!`);
 
         if (MAIN_CONTAINER.querySelector(`div[data-id="${data.messageId}"]`) == null) {
-            $(MAIN_CONTAINER).append(message);
+            $(MAIN_CONTAINER).append(htmlTemplate);
         }
     } else if (event.type === "unichat:raid") {
         const data = event.data
-        let message = RAID_ITEM.replaceAll("{author_id}", data.authorId);
-        message = message.replaceAll("{platform}", data.platform);
-        message = message.replaceAll("{message_id}", data.messageId);
-        message = message.replaceAll("{message}", `${data.authorDisplayName} is raiding with ${data.viewers ?? 'their'} viewers!`);
+        let htmlTemplate = RAID_TEMPLATE.replaceAll("{author_id}", data.authorId);
+        htmlTemplate = htmlTemplate.replaceAll("{platform}", data.platform);
+        htmlTemplate = htmlTemplate.replaceAll("{message_id}", data.messageId);
+        htmlTemplate = htmlTemplate.replaceAll("{message}", `${data.authorDisplayName} is raiding with ${data.viewerCount ?? 'their'} viewers!`);
 
-        $(MAIN_CONTAINER).append(message);
+        $(MAIN_CONTAINER).append(htmlTemplate);
     } else if (event.type === 'unichat:remove_message') {
         MAIN_CONTAINER.querySelector(`div[data-id="${event.data.messageId}"]`)?.remove();
     } else if (event.type === 'unichat:remove_author') {
