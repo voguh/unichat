495de1a4048aff7936a9ae2ca17aa00e2f3f3038
Voguh <voguhofc@protonmail.com>
2025-07-04 17:24:37 -0300

    fix: twitch emotes delimiters

--- src/twitch/mapper/structs/message.rs
+++ src/twitch/mapper/structs/message.rs
@@ -15,8 +15,17 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  ******************************************************************************/
 
+use std::collections::HashSet;
+use std::sync::LazyLock;
+
 use crate::events::unichat::UniChatEmote;
 use crate::shared_emotes;
+
+fn parse_delimiter(start: &str, end: &str) -> Result<(usize, usize), Box<dyn std::error::Error>> {
+    let start = start.parse::<usize>()?;
+    let end = end.parse::<usize>()?;
+    return Ok((start, end));
+}
 
 pub fn parse_message_emotes(raw_emotes: Option<&String>, message_text: &str) -> Result<Vec<UniChatEmote>, Box<dyn std::error::Error>> {
     let mut emotes = Vec::new();
@@ -36,12 +45,11 @@ pub fn parse_message_emotes(raw_emotes: Option<&String>, message_text: &str) -> 
             }
 
             let (emote_id, positions_raw) = raw_emote.split_once(":").ok_or("Invalid emote format")?;
+
+            // Get first delimiter to retrieve emote code
+            let positions_raw = positions_raw.split(",").next().ok_or("Invalid emote positions format")?;
             let (start, end) = positions_raw.split_once("-")
-                .and_then(|(start, end)| {
-                    let start = start.parse::<usize>().ok()?;
-                    let end = end.parse::<usize>().ok()?;
-                    return Some((start, end))
-                })
+                .and_then(|(start, end)| parse_delimiter(start, end).ok())
                 .ok_or("Invalid emote positions format")?;
 
             let emote_code = &message_text[start..end + 1].to_string();
@@ -57,4 +65,26 @@ pub fn parse_message_emotes(raw_emotes: Option<&String>, message_text: &str) -> 
     }
 
     return Ok(emotes);
+}
+
+static CHEER_VARIANTS: LazyLock<HashSet<&'static str>> = LazyLock::new(|| HashSet::from([
+    "cheer", "cheerwhal", "Corgo", "uni", "ShowLove", "Party", "SeemsGood", "Pride", "Kappa", "FrankerZ", "HeyGuys",
+    "DansGame", "TriHard", "Kreygasm", "4Head", "SwiftRage", "NotLikeThis", "FailFish", "VoHiYo", "PJSalt",
+    "MrDestructoid", "bday", "RIPCheer", "Shamrock"
+]));
+
+pub fn parse_message_string(message_raw: &String) -> Result<String, Box<dyn std::error::Error>> {
+    let mut str_message = Vec::new();
+
+    for word in message_raw.split_whitespace() {
+        let prefix_end = word.find(|c: char| c.is_digit(10)).unwrap_or(word.len());
+        let prefix = &word[..prefix_end];
+        let suffix = &word[prefix_end..];
+
+        if !CHEER_VARIANTS.contains(prefix) || suffix.parse::<u32>().is_ok() {
+            str_message.push(word);
+        }
+    }
+
+    return Ok(str_message.join(" "));
 }
