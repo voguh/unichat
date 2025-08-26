a912c300e41aefe79e26e3ef0126fda6d40ec96a
Voguh <voguhofc@protonmail.com>
2025-08-26 17:01:42 -0300

    fix: some license headers

--- webapp/src/components/Dashboard/Dashboard.tsx
+++ webapp/src/components/Dashboard/Dashboard.tsx
@@ -6,6 +6,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  ******************************************************************************/
+
 import React from "react";
 
 import { Badge, Button, Card, Divider, Menu, Tooltip } from "@mantine/core";
