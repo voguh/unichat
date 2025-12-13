5222dce15f13382b29d016fe7477cdcc6ff749f2
Voguh <voguhofc@protonmail.com>
2025-12-12 21:03:35 -0300

    refactor: loggerService

--- webapp/src/services/loggerService.ts
+++ webapp/src/services/loggerService.ts
@@ -9,50 +9,95 @@
 
 import { debug, error, info, trace, warn } from "@tauri-apps/plugin-log";
 
-export class LoggerService {
+export class Logger {
+    private readonly name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
     public trace(message: string, ...args: any[]): void {
-        const formattedMessage = this._formatMessage(message, ...args);
-        trace(formattedMessage).catch((err) => console.error(err));
+        const { formatted, throwable } = this._formatMessage(message, args);
+        trace(formatted, { file: this.name }).catch(console.error);
+        console.trace(formatted);
+
+        if (throwable) {
+            error(throwable.stack, { file: this.name }).catch(console.error);
+            console.error(throwable);
+        }
     }
 
     public debug(message: string, ...args: any[]): void {
-        const formattedMessage = this._formatMessage(message, ...args);
-        debug(formattedMessage).catch((err) => console.error(err));
+        const { formatted, throwable } = this._formatMessage(message, args);
+        debug(formatted, { file: this.name }).catch(console.error);
+        console.debug(formatted);
+
+        if (throwable) {
+            error(throwable.stack, { file: this.name }).catch(console.error);
+            console.error(throwable);
+        }
     }
 
     public info(message: string, ...args: any[]): void {
-        const formattedMessage = this._formatMessage(message, ...args);
-        info(formattedMessage).catch((err) => console.error(err));
+        const { formatted, throwable } = this._formatMessage(message, args);
+        info(formatted, { file: this.name }).catch(console.error);
+        console.info(formatted);
+
+        if (throwable) {
+            error(throwable.stack, { file: this.name }).catch(console.error);
+            console.error(throwable);
+        }
     }
 
     public warn(message: string, ...args: any[]): void {
-        const formattedMessage = this._formatMessage(message, ...args);
-        warn(formattedMessage).catch((err) => console.error(err));
+        const { formatted, throwable } = this._formatMessage(message, args);
+        warn(formatted, { file: this.name }).catch(console.error);
+        console.warn(formatted);
+
+        if (throwable) {
+            error(throwable.stack, { file: this.name }).catch(console.error);
+            console.error(throwable);
+        }
     }
 
     public error(message: string, ...args: any[]): void {
-        const formattedMessage = this._formatMessage(message, ...args);
-        error(formattedMessage).catch((err) => console.error(err));
+        const { formatted, throwable } = this._formatMessage(message, args);
+        error(formatted, { file: this.name }).catch(console.error);
+        console.error(formatted);
 
-        if (args.at(-1) instanceof Error) {
-            const lastArg = args.at(-1) as Error;
-            error(lastArg.stack ?? lastArg.message).catch((err) => console.error(err));
+        if (throwable) {
+            error(throwable.stack, { file: this.name }).catch(console.error);
+            console.error(throwable);
         }
     }
 
-    private _formatMessage(message: string, ...args: any[]): string {
-        if (args.length > 0) {
-            for (const arg of args) {
-                if (arg instanceof Error) {
-                    message = message.replace("{}", arg.message);
-                } else {
-                    message = message.replace("{}", String(arg));
-                }
-            }
+    private _formatMessage(message: string, args: any[]): { formatted: string; throwable: Error | null } {
+        let throwable: Error | null = null;
+        let usedArgs = args;
+
+        if (args.length > 0 && args[args.length - 1] instanceof Error) {
+            throwable = args[args.length - 1];
+            usedArgs = args.slice(0, -1);
         }
 
-        return message;
+        let formatted = message;
+        for (const arg of usedArgs) {
+            formatted = formatted.replace("{}", String(arg));
+        }
+
+        return { formatted, throwable };
     }
 }
 
-export const loggerService = new LoggerService();
+export class LoggerFactory {
+    constructor() {
+        throw new Error("LoggerFactory is a static class and cannot be instantiated.");
+    }
+
+    public static getLogger(name: string): Logger {
+        return new Logger(name);
+    }
+}
+
+/** @deprecated use {@link LoggerFactory.getLogger} instead. */
+export const loggerService = LoggerFactory.getLogger(import.meta.url);
