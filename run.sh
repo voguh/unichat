8119dba957c66ceb6bfbb6163ce054b8c7c60f4b
Voguh <voguhofc@protonmail.com>
2025-12-13 16:52:36 -0300

    fix: development run script install webapp dependencies

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ ROOT_PATH="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 export TAURI_APP_PATH="$ROOT_PATH"
 export TAURI_FRONTEND_PATH="$ROOT_PATH/webapp"
 
-cargo tauri dev
+pnpm --prefix "$TAURI_FRONTEND_PATH" install --frozen-lockfile;
+cargo tauri dev;
