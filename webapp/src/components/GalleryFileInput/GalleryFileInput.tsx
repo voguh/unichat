506e6b14d5eaee90b47b1f26bd4ac691e4d7cefd
Voguh <voguhofc@protonmail.com>
2025-12-29 17:04:24 -0300

    fix: add some license headers

--- webapp/src/components/GalleryFileInput/GalleryFileInput.tsx
+++ webapp/src/components/GalleryFileInput/GalleryFileInput.tsx
@@ -1,3 +1,12 @@
+/*!******************************************************************************
+ * UniChat
+ * Copyright (C) 2025 Voguh <voguhofc@protonmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ ******************************************************************************/
+
 import React from "react";
 
 import { TextInput, TextInputProps } from "@mantine/core";
