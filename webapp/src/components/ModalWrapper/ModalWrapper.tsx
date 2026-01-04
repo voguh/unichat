eed97bd621fb68c0bfa8a117268c34302865b124
Voguh <voguhofc@protonmail.com>
2026-01-03 21:21:55 -0300

    feat: implement modal wrapper shared context

--- webapp/src/components/ModalWrapper/ModalWrapper.tsx
+++ webapp/src/components/ModalWrapper/ModalWrapper.tsx
@@ -21,59 +21,83 @@ type Props = ContextModalProps<{
     leftSection?: React.ReactNode;
     leftSectionTitle?: string;
     modalProps?: ModalProps & { modalId?: string };
+    sharedStoreInitialState?: Record<string, any>;
     title: string;
 }>;
 
-export function ModalWrapper({ id, innerProps }: Props): React.ReactNode {
-    const { actions, children, leftSection, leftSectionTitle, modalProps, title } = innerProps;
+export interface ModalContextType {
+    modalProps: ModalProps & { modalId?: string };
+    onClose: () => void;
+    setSharedStore: React.Dispatch<React.SetStateAction<Record<string, any>>>;
+    sharedStore: Record<string, any>;
+}
 
+export const ModalContext = React.createContext({} as ModalContextType);
+
+export function ModalWrapper({ id, innerProps }: Props): React.ReactNode {
+    const { actions, children, leftSection, leftSectionTitle, modalProps, sharedStoreInitialState, title } = innerProps;
+
+    const [sharedStore, setSharedStore] = React.useState<Record<string, any>>(sharedStoreInitialState || {});
     const wrapperRef = React.useRef<HTMLDivElement>(null);
 
+    const safeActions = sharedStore.modalActions || actions;
+    const safeModalProps = (modalProps || { modalId: id }) as ModalProps & { modalId?: string };
+    const safeTitle = sharedStore.modalTitle || title;
+    const safeLeftSectionTitle = sharedStore.leftSectionTitle || leftSectionTitle;
+
+    function onClose(): void {
+        modals.close(id);
+    }
+
     React.useEffect(() => {
-        let isFullScreen = false;
+        if (wrapperRef.current) {
+            let isFullScreen = false;
 
-        if (modalProps != null) {
-            isFullScreen = modalProps.fullScreen ?? false;
-        } else if (wrapperRef.current) {
-            const modalParent = wrapperRef.current.closest(".mantine-Modal-root");
+            if (safeModalProps != null) {
+                isFullScreen = safeModalProps.fullScreen ?? false;
+            } else {
+                const modalParent = wrapperRef.current.closest(".mantine-Modal-root");
 
-            if (modalParent) {
-                const isFullscreen = modalParent.getAttribute("data-full-screen") === "true";
-                if (isFullscreen) {
-                    wrapperRef.current.classList.add("is-fullscreen");
-                } else {
-                    wrapperRef.current.classList.remove("is-fullscreen");
+                if (modalParent) {
+                    const isFullscreen = modalParent.getAttribute("data-full-screen") === "true";
+                    if (isFullscreen) {
+                        wrapperRef.current.classList.add("is-fullscreen");
+                    } else {
+                        wrapperRef.current.classList.remove("is-fullscreen");
+                    }
                 }
             }
-        }
 
-        if (isFullScreen) {
-            wrapperRef.current?.classList.add("is-fullscreen");
-        } else {
-            wrapperRef.current?.classList.remove("is-fullscreen");
+            if (isFullScreen) {
+                wrapperRef.current.classList.add("is-fullscreen");
+            } else {
+                wrapperRef.current.classList.remove("is-fullscreen");
+            }
         }
     }, [id]);
 
     return (
-        <ModalWrapperStyledContainer ref={wrapperRef} className={clsx({ "with-sidebar": !!leftSection })}>
-            {leftSection && (
-                <div className="modal-wrapper-sidebar">
-                    <div className="modal-wrapper-sidebar-header">{leftSectionTitle}</div>
-                    <div className="modal-wrapper-sidebar-content">{leftSection}</div>
-                </div>
-            )}
-            <div className="modal-wrapper-content">
-                <div className="modal-wrapper-header">
-                    <div className="modal-wrapper-header--title">{title}</div>
-                    <div className="modal-wrapper-header--actions">
-                        {actions}
-                        <Button variant="default" onClick={() => modals.close(id)}>
-                            <i className="fas fa-times" />
-                        </Button>
+        <ModalContext.Provider value={{ modalProps: safeModalProps, onClose, setSharedStore, sharedStore }}>
+            <ModalWrapperStyledContainer ref={wrapperRef} className={clsx({ "with-sidebar": !!leftSection })}>
+                {leftSection && (
+                    <div className="modal-wrapper-sidebar">
+                        <div className="modal-wrapper-sidebar-header">{safeLeftSectionTitle}</div>
+                        <div className="modal-wrapper-sidebar-content">{leftSection}</div>
                     </div>
+                )}
+                <div className="modal-wrapper-content">
+                    <div className="modal-wrapper-header">
+                        <div className="modal-wrapper-header--title">{safeTitle}</div>
+                        <div className="modal-wrapper-header--actions">
+                            {safeActions}
+                            <Button variant="default" onClick={onClose}>
+                                <i className="fas fa-times" />
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="modal-wrapper-body">{children}</div>
                 </div>
-                <div className="modal-wrapper-body">{children}</div>
-            </div>
-        </ModalWrapperStyledContainer>
+            </ModalWrapperStyledContainer>
+        </ModalContext.Provider>
     );
 }
--- webapp/src/services/modalService.ts
+++ webapp/src/services/modalService.ts
@@ -11,16 +11,17 @@ import { modals } from "@mantine/modals";
 import { OpenContextModal } from "node_modules/@mantine/modals/lib/context";
 
 export interface OpenModalProps extends Omit<OpenContextModal, "modal" | "innerProps"> {
-    title: string;
-    leftSectionTitle?: string;
-    leftSection?: React.ReactNode;
     actions?: React.ReactNode;
     children: React.ReactNode;
+    leftSection?: React.ReactNode;
+    leftSectionTitle?: string;
+    sharedStoreInitialState?: Record<string, any>;
+    title: string;
 }
 
 export class ModalService {
     openModal(opts: OpenModalProps): string {
-        const { title, leftSection, leftSectionTitle, actions, children, ...modalProps } = opts;
+        const { actions, children, leftSection, leftSectionTitle, sharedStoreInitialState, title, ...rest } = opts;
         if ("modal" in opts) {
             delete opts.modal;
         }
@@ -30,11 +31,19 @@ export class ModalService {
         }
 
         return modals.openContextModal({
-            ...modalProps,
+            ...rest,
             modal: "unichat",
             withCloseButton: false,
             centered: true,
-            innerProps: { actions, children, leftSection, leftSectionTitle, modalProps, title }
+            innerProps: {
+                actions: actions,
+                children: children,
+                leftSection: leftSection,
+                leftSectionTitle: leftSectionTitle,
+                modalProps: rest,
+                sharedStoreInitialState: sharedStoreInitialState,
+                title: title
+            }
         });
     }
 }
