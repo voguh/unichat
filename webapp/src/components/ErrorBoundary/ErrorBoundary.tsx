41905139ab747e471481e4312b38eeee53ecbd6f
Voguh <voguhofc@protonmail.com>
2026-02-08 19:19:38 -0300

    feat: dummy error boundary

---
+++ webapp/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,59 @@
+/*!******************************************************************************
+ * Copyright (c) 2026 Voguh
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
+
+import React from "react";
+
+import { LoggerFactory } from "unichat/logging/LoggerFactory";
+
+import { ErrorBoundaryStyledContainer } from "./styled";
+
+interface Props {
+    children?: React.ReactNode;
+}
+
+interface State {
+    error: Error | null;
+    errorInfo: React.ErrorInfo | null;
+}
+
+const _logger = LoggerFactory.getLogger("ErrorBoundary");
+export class ErrorBoundary extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            error: null,
+            errorInfo: null
+        };
+    }
+
+    public componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+        this.setState({ error, errorInfo });
+    }
+
+    public static getDerivedStateFromError(error: Error): Partial<State> {
+        return { error };
+    }
+
+    public render(): React.ReactNode {
+        const { error, errorInfo } = this.state;
+
+        if (error) {
+            return (
+                <ErrorBoundaryStyledContainer>
+                    Error: {error.message}
+                    Component Stack:
+                    {errorInfo?.componentStack || "No component stack available."}
+                </ErrorBoundaryStyledContainer>
+            );
+        }
+
+        return this.props.children;
+    }
+}
