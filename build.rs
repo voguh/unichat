113f4be3c10c2bbbb20f9b51f00c643b70ae27cb
Voguh <voguhofc@protonmail.com>
2026-04-14 15:40:57 -0300

    refactor: build job to prevent external http call

--- build.rs
+++ build.rs
@@ -13,7 +13,6 @@
 #![allow(clippy::redundant_field_names)]
 
 use std::collections::HashMap;
-use std::collections::HashSet;
 use std::env;
 use std::fs;
 use std::hash::DefaultHasher;
@@ -22,10 +21,6 @@ use std::hash::Hasher as _;
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
-
-use ureq::config::Config;
-use ureq::tls::TlsConfig;
-use ureq::tls::TlsProvider;
 
 // I will not implement any api to get license information.
 // This is a "safe guard" to remember me to change license info if I change the license.
@@ -79,62 +74,57 @@ struct FinalLicenseInfo {
     source: String,
     name: String,
     version: String,
-    authors: HashSet<String>,
     repository: String,
     licenses: String
 }
 
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
+struct CargoPackage {
+    name: String,
+    version: String,
+    license: String,
+
+    dependencies: Vec<CargoDependency>
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
+struct CargoDependency {
+    name: String,
+    kind: Option<String>
+}
+
 fn generate_crate_licenses_info() -> Result<Vec<FinalLicenseInfo>, Box<dyn std::error::Error>> {
     let mut final_licenses: Vec<FinalLicenseInfo> = Vec::new();
-    let mut cargo_bin = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
-    if cargo_bin.contains("cargo-xwin") || cargo_bin.contains("cargo-win") {
-        cargo_bin = String::from("cargo");
+
+    let cargo_bin = env::var("CARGO_HOME").map(|h| format!("{}/bin/cargo", h)).unwrap_or_else(|_| "cargo".to_string());
+    let metadata_output = Command::new(&cargo_bin).args(["metadata", "--format-version", "1"]).output()?;
+    if !metadata_output.status.success() {
+        return Err(format!("Failed to run metadata: {}", String::from_utf8_lossy(&metadata_output.stderr)).into());
     }
 
-    let metadata = Command::new(&cargo_bin)
-        .args(["metadata", "--format-version", "1"])
-        // Remove env vars that may force cross-target flags into rustc.
-        .env_remove("RUSTFLAGS")
-        .env_remove("CARGO_ENCODED_RUSTFLAGS")
-        .env_remove("CARGO")
-        .output()?;
-    if !metadata.status.success() {
-        return Err(format!("Failed to run metadata: {}", String::from_utf8_lossy(&metadata.stderr)).into());
-    }
+    let cargo_metadata_json = String::from_utf8(metadata_output.stdout)?;
+    let cargo_metadata: serde_json::Value = serde_json::from_str(&cargo_metadata_json)?;
 
-    let cargo_manifest_json = String::from_utf8(metadata.stdout)?;
-    let cargo_manifest: serde_json::Value = serde_json::from_str(&cargo_manifest_json)?;
+    let packages: Vec<CargoPackage> = serde_json::from_value(cargo_metadata["packages"].clone())?;
+    let packages_map: HashMap<String, CargoPackage> = packages.into_iter()
+        .map(|pkg| (pkg.name.clone(), pkg.clone()))
+        .collect();
 
-    let packages = cargo_manifest["packages"].as_array().ok_or("missing packages")?;
-    let packages_map: HashMap<String, serde_json::Value> = packages.iter().map(|pkg| {
-        return (pkg["name"].as_str().unwrap_or_default().to_string(), pkg.clone());
-    }).collect();
+    // let unichat_direct_deps = unichat_package["dependencies"].as_array().ok_or("missing dependencies")?;
 
     let unichat_package = packages_map["unichat"].clone();
-    let unichat_direct_deps = unichat_package["dependencies"].as_array().ok_or("missing dependencies")?;
-
-    for dep in unichat_direct_deps {
-        let dep_name = dep["name"].as_str().ok_or("missing dependency.name")?;
-        if let Some(kind) = dep["kind"].as_str() {
-            if kind == "dev" || kind == "build" {
-                continue;
-            }
+    for dep in unichat_package.dependencies {
+        if matches!(dep.kind.as_deref(), Some("dev") | Some("build")) {
+            continue;
         }
 
-        if let Some(package) = packages_map.get(dep_name) {
-            let name = package["name"].as_str().ok_or("missing package.name")?;
-            let version = package["version"].as_str().ok_or("missing package.version")?;
-            let authors: HashSet<String> = serde_json::from_value(package["authors"].clone())?;
-            let repository = format!("https://crates.io/crates/{}/{}", &name, &version);
-            let license = package["license"].as_str().ok_or("missing package.license")?;
-
+        if let Some(package) = packages_map.get(&dep.name) {
             final_licenses.push(FinalLicenseInfo {
                 source: String::from("crate"),
-                name: String::from(name),
-                version: String::from(version),
-                authors: authors,
-                repository: repository,
-                licenses: String::from(license)
+                name: package.name.clone(),
+                version: package.version.clone(),
+                repository: format!("https://crates.io/crates/{}/{}", package.name, package.version),
+                licenses: package.license.clone()
             });
         }
     }
@@ -142,161 +132,95 @@ fn generate_crate_licenses_info() -> Result<Vec<FinalLicenseInfo>, Box<dyn std::
     return Ok(final_licenses);
 }
 
+/* ========================================================================== */
 
-fn generate_npm_authors(package: serde_json::Value) -> Result<HashSet<String>, Box<dyn std::error::Error>> {
-    let mut authors: HashSet<String> = HashSet::new();
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
+struct PNPMPackage {
+    name: String,
+    dependencies: HashMap<String, PNPMDependency>
+}
 
-    if let Some(author) = package.get("author") {
-        if let Some(author_str) = author.as_str() {
-            authors.insert(author_str.to_string());
-        } else if let Some(author_obj) = author.as_object() {
-            let name = author_obj.get("name").and_then(|v| v.as_str()).ok_or("missing author.name")?;
-            let email = author_obj.get("email").and_then(|v| v.as_str()).unwrap_or("");
-
-            if email.is_empty() {
-                authors.insert(name.to_string());
-            } else {
-                authors.insert(format!("{} <{}>", name, email));
-            }
-        }
-    }
-
-    if let Some(contributors) = package["contributors"].as_array() {
-        for contributor in contributors {
-            if let Some(contributor_str) = contributor.as_str() {
-                authors.insert(contributor_str.to_string());
-            } else if let Some(contributor_obj) = contributor.as_object() {
-                let name = contributor_obj.get("name").and_then(|v| v.as_str()).ok_or("missing author.name")?;
-                let email = contributor_obj.get("email").and_then(|v| v.as_str()).unwrap_or("");
-
-                if email.is_empty() {
-                    authors.insert(name.to_string());
-                } else {
-                    authors.insert(format!("{} <{}>", name, email));
-                }
-            }
-        }
-    }
-
-    if authors.is_empty() {
-        if let Some(maintainers) =package["maintainers"].as_array() {
-            for maintainer in maintainers {
-                if let Some(contributor_str) = maintainer.as_str() {
-                    authors.insert(contributor_str.to_string());
-                } else if let Some(contributor_obj) = maintainer.as_object() {
-                    let name = contributor_obj.get("name").and_then(|v| v.as_str()).ok_or("missing author.name")?;
-                    let email = contributor_obj.get("email").and_then(|v| v.as_str()).unwrap_or("");
-
-                    if email.is_empty() {
-                        authors.insert(name.to_string());
-                    } else {
-                        authors.insert(format!("{} <{}>", name, email));
-                    }
-                }
-            }
-        }
-    }
-
-    return Ok(authors);
+#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
+struct PNPMDependency {
+    #[serde(rename = "from")]
+    name: String,
+    version: String,
+    license: String
 }
 
 fn generate_npm_licenses_info() -> Result<Vec<FinalLicenseInfo>, Box<dyn std::error::Error>> {
-    let package_json_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("webapp").join("package.json");
-    if !package_json_path.exists() {
-        return Err(format!("package.json not found at {}", package_json_path.display()).into());
-    }
-
-    let package_json_content = fs::read_to_string(package_json_path)?;
-    let package_json: serde_json::Value = serde_json::from_str(&package_json_content)?;
-    let dependencies = package_json["dependencies"].as_object().ok_or("missing dependencies")?;
-
     let mut final_licenses: Vec<FinalLicenseInfo> = Vec::new();
 
-    for (raw_name, raw_version) in dependencies {
-        let raw_version = raw_version.as_str().ok_or("dependency version is not a string")?;
-        let npm_url = format!("https://registry.npmjs.org/{}/{}", raw_name, raw_version);
-
-        let config = Config::builder()
-            .tls_config(
-                TlsConfig::builder()
-                    .provider(TlsProvider::NativeTls)
-                    .root_certs(ureq::tls::RootCerts::PlatformVerifier)
-                    .build()
-            )
-            .build();
-
-        let mut response = config.new_agent().get(npm_url).call()?;
-        let package: serde_json::Value = response.body_mut().read_json()?;
-
-        let name = package["name"].as_str().ok_or("missing package.name")?;
-        let version = package["version"].as_str().ok_or("missing package.version")?;
-        let authors: HashSet<String> = generate_npm_authors(package.clone())?;
-        let repository = format!("https://www.npmjs.com/package/{}/v/{}", &name, &version);
-
-        let license: &str;
-        if name == "@tabler/icons-webfont" && version == "3.36.1" {
-            // NPM package "@tabler/icons-webfont" doesn't have a license field, but on the npmjs
-            // page it says that the project is under the MIT license.
-            license = "MIT";
-        } else {
-            license = package["license"].as_str().ok_or("missing package.license")?;
-        }
-
-        final_licenses.push(FinalLicenseInfo {
-            source: String::from("npm"),
-            name: String::from(name),
-            version: String::from(version),
-            authors: authors,
-            repository: repository,
-            licenses: String::from(license)
-        });
+    let webapp_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("webapp");
+    let pnpm_bin = env::var("PNPM_HOME").map(|h| format!("{}/pnpm", h)).unwrap_or_else(|_| "pnpm".to_string());
+    let metadata_output = Command::new(&pnpm_bin).args(["list", "--json", "--long", "--prod"]).current_dir(&webapp_path).output()?;
+    if !metadata_output.status.success() {
+        return Err(format!("Failed to run metadata: {}", String::from_utf8_lossy(&metadata_output.stderr)).into());
     }
 
+    let pnpm_metadata_json = String::from_utf8(metadata_output.stdout)?;
+    let pnpm_metadata: Vec<PNPMPackage> = serde_json::from_str(&pnpm_metadata_json)?;
+
+    let packages_map: HashMap<String, PNPMPackage> = pnpm_metadata.into_iter()
+        .map(|pkg| (pkg.name.clone(), pkg.clone()))
+        .collect();
+
+    let unichat_package = packages_map["unichat"].clone();
+    for dep in unichat_package.dependencies.values() {
+        final_licenses.push(FinalLicenseInfo {
+            source: String::from("npm"),
+            name: dep.name.clone(),
+            version: dep.version.clone(),
+            repository: format!("https://www.npmjs.com/package/{}/v/{}", dep.name, dep.version),
+            licenses: dep.license.clone()
+        });
+    }
 
     return Ok(final_licenses);
 }
 
-fn calculate_hash(content: &str) -> u64 {
+fn calculate_hash(content: &[u8]) -> u64 {
     let mut hasher = DefaultHasher::new();
     content.hash(&mut hasher);
 
     return hasher.finish();
 }
 
-fn calculate_third_party_licenses_lock(lock_file_path: &Path) -> Result<bool, Box<dyn std::error::Error>> {
+fn generate_third_party_licenses_lock_hash() -> Result<String, Box<dyn std::error::Error>> {
     let cargo_lock_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("Cargo.lock");
     let pnpm_lock_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("webapp").join("pnpm-lock.yaml");
 
-    let cargo_content = fs::read_to_string(&cargo_lock_path)?;
-    let pnpm_content = fs::read_to_string(&pnpm_lock_path)?;
-    let combined_content = cargo_content + &pnpm_content;
+    let cargo_content = fs::read(&cargo_lock_path)?;
+    let pnpm_content = fs::read(&pnpm_lock_path)?;
+    let combined_content = [cargo_content, pnpm_content].concat();
 
     let current_hash = calculate_hash(&combined_content);
-
-    let mut saved_hash = 0;
-    if lock_file_path.exists() {
-        let content = fs::read_to_string(&lock_file_path)?;
-        saved_hash = content.trim().parse().unwrap_or(0);
-    }
-
-    if current_hash == saved_hash {
-        return Ok(true);
-    } else {
-        fs::write(lock_file_path, current_hash.to_string())?;
-        return Ok(false);
-    }
+    return Ok(current_hash.to_string());
 }
 
 fn generate_third_party_licenses(target_gen_dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
-    let licenses_file_path = target_gen_dir.join("third_party_licenses.json");
+    let current_hash = generate_third_party_licenses_lock_hash()?;
 
+    let licenses_file_path = target_gen_dir.join("third_party_licenses.json");
     let licenses_lock_file_path = target_gen_dir.join("third_party_licenses.lock");
-    if calculate_third_party_licenses_lock(&licenses_lock_file_path)? {
-        return Ok(());
+    let file_exists = licenses_file_path.exists();
+    let lock_exists = licenses_lock_file_path.exists();
+
+    // Remove the licenses file or lock file if only one of them exists, to avoid inconsistency.
+    if file_exists != lock_exists {
+        if file_exists {
+            fs::remove_dir(&licenses_file_path)?;
+        } else {
+            fs::remove_file(&licenses_lock_file_path)?;
+        }
     }
 
-    if fs::exists(&licenses_file_path)? {
-        fs::remove_file(&licenses_file_path)?;
+    if lock_exists {
+        let existing_hash = fs::read_to_string(&licenses_lock_file_path)?;
+
+        if existing_hash == current_hash {
+            return Ok(());
+        }
     }
 
     let mut final_licenses: Vec<FinalLicenseInfo> = Vec::new();
@@ -309,6 +233,7 @@ fn generate_third_party_licenses(target_gen_dir: &Path) -> Result<(), Box<dyn st
 
     let final_licenses_json = serde_json::to_string(&final_licenses)?;
     fs::write(licenses_file_path, final_licenses_json)?;
+    fs::write(licenses_lock_file_path, current_hash)?;
 
     return Ok(());
 }
