68599b6989548698b11028397af05df421eac277
Voguh <voguhofc@protonmail.com>
2026-04-27 16:58:40 -0300

    fix(webapp): toWidgetOptionGroup types

--- webapp/src/utils/toWidgetOptionGroup.ts
+++ webapp/src/utils/toWidgetOptionGroup.ts
@@ -1,9 +1,19 @@
-import { GroupBase, Option } from "unichat/components/forms/Select";
+/*!******************************************************************************
+ * Copyright (c) 2026 Voguh
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
+
+import { Option, OptionGroupBase } from "unichat/components/forms/Select";
 import { UniChatWidget } from "unichat/types";
 
 import { isSystemWidget, isUserWidget } from "./widgetSourceTypeGuards";
 
-export function toWidgetOptionGroup(widgets: Iterable<UniChatWidget>): GroupBase<Option>[] {
+export function toWidgetOptionGroup(widgets: Iterable<UniChatWidget>): OptionGroupBase<Option>[] {
     const systemWidgets = [];
     const userWidgets = [];
     const pluginWidgets = [];
@@ -18,7 +28,7 @@ export function toWidgetOptionGroup(widgets: Iterable<UniChatWidget>): GroupBase
         }
     }
 
-    const options: GroupBase<Option>[] = [];
+    const options: OptionGroupBase<Option>[] = [];
 
     if (systemWidgets.length > 0) {
         options.push({
