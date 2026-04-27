1d8d7f5c772d6c42ae821e2328d57c68980e0c64
Voguh <voguhofc@protonmail.com>
2026-04-27 13:45:04 -0300

    fix(webapp): button size

--- webapp/src/components/Button/styled.ts
+++ webapp/src/components/Button/styled.ts
@@ -21,7 +21,9 @@ interface Props {
 
 export const ButtonStyledContainer = styled.button((props: Props) => {
     const styles = {
-        ...tw`px-4 py-2 rounded focus:outline-none transition-colors duration-200 border border-stone-700`
+        ...tw`px-4 py-2 rounded focus:outline-none transition-colors duration-200 border border-stone-700`,
+        height: "36px",
+        minWidth: "36px"
     };
 
     switch (props.variant) {
