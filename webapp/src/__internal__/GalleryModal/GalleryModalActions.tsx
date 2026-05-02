5d1e877d02fd7eb6978efc96d06d13268cc6d57b
Voguh <voguhofc@protonmail.com>
2026-05-02 14:50:32 -0300

    feat(webapp): implement add to gallery in GalleryModalActions

--- webapp/src/__internal__/GalleryModal/GalleryModalActions.tsx
+++ webapp/src/__internal__/GalleryModal/GalleryModalActions.tsx
@@ -9,16 +9,46 @@
  ******************************************************************************/
 
 import * as PReact from "preact";
+import { useContext } from "preact/hooks";
 
+import * as dialog from "@tauri-apps/plugin-dialog";
 import { revealItemInDir } from "@tauri-apps/plugin-opener";
 
 import { Button } from "unichat/components/Button";
+import { ModalContext } from "unichat/contexts/ModalContext";
+import { LoggerFactory } from "unichat/logging/LoggerFactory";
+import { commandService } from "unichat/services/commandService";
 
+const _logger = LoggerFactory.getLogger("GalleryModalActions");
 export function GalleryModalActions(): PReact.ComponentChildren {
+    const { setSharedStore } = useContext(ModalContext);
+
+    async function onFilesUploadClick(): Promise<void> {
+        try {
+            setSharedStore((old) => ({ ...old, uploading: true }));
+            const response = await dialog.open({ multiple: true, directory: false });
+            if (response == null || !Array.isArray(response)) {
+                throw new Error("Invalid response from file dialog");
+            }
+
+            await commandService.uploadGalleryItems(response);
+        } catch (error) {
+            _logger.error("An error occurred on upload gallery items", error);
+        } finally {
+            setSharedStore((old) => ({ ...old, uploading: false }));
+        }
+    }
+
     return (
-        <Button variant="secondary" onClick={() => revealItemInDir(UNICHAT_GALLERY_DIR)}>
-            <i className="fas fa-folder" />
-            Show Gallery Folder
-        </Button>
+        <>
+            <Button variant="secondary" onClick={onFilesUploadClick}>
+                <i className="fas fa-upload" />
+                Add to Gallery
+            </Button>
+            <Button variant="secondary" onClick={() => revealItemInDir(UNICHAT_GALLERY_DIR)}>
+                <i className="fas fa-folder" />
+                Show Gallery Folder
+            </Button>
+        </>
     );
 }
