7c683a59d9682ef240bf3e83e77d5c19de0b672b
Voguh <voguhofc@protonmail.com>
2026-05-10 17:22:25 -0300

    fix(webapp): button hover style when disable

--- webapp/src/components/Button/styled.ts
+++ webapp/src/components/Button/styled.ts
@@ -13,65 +13,63 @@ import { ButtonHTMLAttributes, ComponentType } from "preact";
 import { styled } from "goober";
 import tw from "twin.macro";
 
-export const ButtonStyledContainer: ComponentType<ButtonHTMLAttributes<HTMLButtonElement>> = styled.button((props) => {
-    return {
-        ...tw`px-4 py-2 rounded bg-stone-900 border border-stone-700 text-stone-50 flex justify-center items-center gap-2 font-semibold`,
-        height: "36px",
+export const ButtonStyledContainer: ComponentType<ButtonHTMLAttributes<HTMLButtonElement>> = styled.button({
+    ...tw`px-4 py-2 rounded bg-stone-900 border border-stone-700 text-stone-50 flex justify-center items-center gap-2 font-semibold`,
+    height: "36px",
 
-        "&:hover": {
-            ...tw`bg-stone-800 border-stone-700`
-        },
+    "&:hover:not(:disabled)": {
+        ...tw`bg-stone-800 border-stone-700`
+    },
 
-        "&[data-variant=primary]": {
-            ...tw`bg-red-800 text-stone-50 border-red-800`,
+    "&[data-variant=primary]": {
+        ...tw`bg-red-800 text-stone-50 border-red-800`,
 
-            "&:hover": {
-                ...tw`bg-red-900 border-red-900`
-            }
-        },
-
-        "&[data-variant=secondary]": {
-            ...tw`bg-zinc-700 text-stone-50 border-zinc-700`,
-
-            "&:hover": {
-                ...tw`bg-zinc-800 border-zinc-800`
-            }
-        },
-
-        "&[data-variant=success]": {
-            ...tw`bg-green-700 text-stone-50 border-green-700`,
-
-            "&:hover": {
-                ...tw`bg-green-800 border-green-800`
-            }
-        },
-
-        "&[data-variant=danger]": {
-            ...tw`bg-rose-700 text-stone-50 border-rose-700`,
-
-            "&:hover": {
-                ...tw`bg-rose-800 border-rose-800`
-            }
-        },
-
-        "&[data-variant=warning]": {
-            ...tw`bg-yellow-500 text-black border-yellow-500`,
-
-            "&:hover": {
-                ...tw`bg-yellow-600 border-yellow-600`
-            }
-        },
-
-        "&[data-variant=info]": {
-            ...tw`bg-cyan-700 text-stone-50 border-cyan-700`,
-
-            "&:hover": {
-                ...tw`bg-cyan-800 border-cyan-800`
-            }
-        },
-
-        "&:disabled": {
-            ...tw`bg-stone-700 border-stone-700 text-stone-500 cursor-not-allowed`
+        "&:hover:not(:disabled)": {
+            ...tw`bg-red-900 border-red-900`
         }
-    };
+    },
+
+    "&[data-variant=secondary]": {
+        ...tw`bg-zinc-700 text-stone-50 border-zinc-700`,
+
+        "&:hover:not(:disabled)": {
+            ...tw`bg-zinc-800 border-zinc-800`
+        }
+    },
+
+    "&[data-variant=success]": {
+        ...tw`bg-green-700 text-stone-50 border-green-700`,
+
+        "&:hover:not(:disabled)": {
+            ...tw`bg-green-800 border-green-800`
+        }
+    },
+
+    "&[data-variant=danger]": {
+        ...tw`bg-rose-700 text-stone-50 border-rose-700`,
+
+        "&:hover:not(:disabled)": {
+            ...tw`bg-rose-800 border-rose-800`
+        }
+    },
+
+    "&[data-variant=warning]": {
+        ...tw`bg-yellow-500 text-black border-yellow-500`,
+
+        "&:hover:not(:disabled)": {
+            ...tw`bg-yellow-600 border-yellow-600`
+        }
+    },
+
+    "&[data-variant=info]": {
+        ...tw`bg-cyan-700 text-stone-50 border-cyan-700`,
+
+        "&:hover:not(:disabled)": {
+            ...tw`bg-cyan-800 border-cyan-800`
+        }
+    },
+
+    "&:disabled": {
+        ...tw`bg-stone-700 border-stone-700 text-stone-500 cursor-not-allowed`
+    }
 });
