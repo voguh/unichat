865dd95615a91d9505debd0babec91fe1e1bd2f8
Voguh <voguhofc@protonmail.com>
2026-05-19 16:26:14 -0300

    fix(webapp): select dropdown scroll

--- webapp/src/components/forms/Select/styled.ts
+++ webapp/src/components/forms/Select/styled.ts
@@ -16,6 +16,8 @@ import tw from "twin.macro";
 import { inputGeneralStyle } from "unichat/components/forms/FormGroup";
 
 export const SelectStyledContainer: ComponentType<HTMLAttributes<HTMLDivElement>> = styled.div({
+    ...tw`cursor-pointer`,
+
     "> input": {
         display: "none"
     },
@@ -38,6 +40,6 @@ export const SelectStyledContainer: ComponentType<HTMLAttributes<HTMLDivElement>
 });
 
 export const SelectStyledDropdown: ComponentType<HTMLAttributes<HTMLDivElement>> = styled.div({
-    ...tw`w-full p-2 rounded border border-stone-700 bg-stone-800/95 shadow-xl backdrop-blur-sm`,
+    ...tw`w-full p-2 rounded border border-stone-700 bg-stone-800/95 shadow-xl overflow-auto backdrop-blur-sm`,
     maxHeight: "calc(32px * 6)"
 });
