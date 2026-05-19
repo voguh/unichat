3b16657768057c1bbbd055a30eb9fdc82629ef57
Voguh <voguhofc@protonmail.com>
2026-05-18 23:17:13 -0300

    refactor(webapp): update tooltip caret placement

--- webapp/src/components/Tooltip/Tooltip.tsx
+++ webapp/src/components/Tooltip/Tooltip.tsx
@@ -10,7 +10,7 @@
 
 import * as PReact from "preact";
 
-import { offset, flip, shift, Placement } from "@floating-ui/dom";
+import { offset, flip, shift, Side, ComputePositionReturn } from "@floating-ui/dom";
 
 import { Portal } from "unichat/components/Portal";
 import { useComputePosition } from "unichat/hooks/useComputePosition";
@@ -20,18 +20,43 @@ import { TooltipStyledContainer } from "./styled";
 
 interface Props {
     maxWidth?: number;
-    placement?: Placement;
+    placement?: Side;
     content: PReact.ComponentChildren;
     children: PReact.VNode;
+}
+
+function adjustFloatingPosition(
+    reference: Element,
+    floating: HTMLElement,
+    { placement, x, y }: ComputePositionReturn
+): [number, number] {
+    const dialogCaretElem = floating.querySelector<HTMLDivElement>(".tooltip-caret");
+    if (dialogCaretElem != null) {
+        const bounds = reference.getBoundingClientRect();
+
+        dialogCaretElem.setAttribute("data-placement", placement);
+        if (placement === "top" || placement === "bottom") {
+            dialogCaretElem.style.left = `${bounds.x + bounds.width / 2}px`;
+            dialogCaretElem.style.top = placement === "top" ? `${bounds.y}px` : `${bounds.y + bounds.height}px`;
+        } else if (placement === "left" || placement === "right") {
+            dialogCaretElem.style.left = placement === "left" ? `${bounds.x}px` : `${bounds.x + bounds.width}px`;
+            dialogCaretElem.style.top = `${bounds.y + bounds.height / 2}px`;
+        }
+    }
+
+    return [x, y];
 }
 
 export function Tooltip({ children, content, maxWidth, placement }: Props): PReact.ComponentChildren {
     const resolvedPlacement = placement || "top";
 
-    const [wrapperRef, tooltipRef, updateVisualization] = useComputePosition<Element, HTMLDivElement>({
-        placement: resolvedPlacement,
-        middleware: [offset(8), flip(), shift({ padding: 8 })]
-    });
+    const [wrapperRef, tooltipRef, updateVisualization] = useComputePosition<Element, HTMLDivElement>(
+        {
+            placement: resolvedPlacement,
+            middleware: [offset(8), flip(), shift({ padding: 8 })]
+        },
+        adjustFloatingPosition
+    );
 
     function show(): void {
         updateVisualization(true);
@@ -61,7 +86,8 @@ export function Tooltip({ children, content, maxWidth, placement }: Props): PRea
                 }}
             >
                 <TooltipStyledContainer data-placement={resolvedPlacement} style={{ maxWidth: maxWidth ?? "300px" }}>
-                    {content}
+                    <div className="tooltip-caret" />
+                    <div className="tooltip-content">{content}</div>
                 </TooltipStyledContainer>
             </Portal>
         </>
