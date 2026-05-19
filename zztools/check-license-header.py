2ca957029ff5b1dce741391216cb9582e390a8a9
Voguh <voguhofc@protonmail.com>
2026-05-19 01:24:37 -0300

    refactor: reimplement check-license-header

--- zztools/check-license-header.py
+++ zztools/check-license-header.py
@@ -10,107 +10,143 @@
 # ******************************************************************************
 
 from datetime import datetime, timezone
+from enum import Enum
 import fnmatch
 import os
 from pathlib import Path
 import re
+import sys
 from utils import logger
 from utils.constants import ROOT_PATH
 
 # ============================================================================ #
 
-def is_text_file(filepath):
+EXCLUDE_PATHS = {
+    ROOT_PATH / ".forgejo",
+    ROOT_PATH / ".git",
+    ROOT_PATH / ".vscode",
+    ROOT_PATH / "(un)released-notes",
+    ROOT_PATH / "capabilities",
+    ROOT_PATH / "dist",
+    ROOT_PATH / "gen",
+    ROOT_PATH / "plugins",
+    ROOT_PATH / "target",
+    ROOT_PATH / "webapp" / "coverage",
+    ROOT_PATH / "webapp" / "dist",
+    ROOT_PATH / "webapp" / "node_modules",
+    ROOT_PATH / "webapp" / "public" / "fontawesome",
+    ROOT_PATH / "webapp" / ".eslintignore",
+    ROOT_PATH / "webapp" / ".eslintrc.json",
+    ROOT_PATH / "webapp" / ".npmrc",
+    ROOT_PATH / "webapp" / ".nvmrc",
+    ROOT_PATH / "webapp" / ".prettierrc.json",
+    ROOT_PATH / "webapp" / "package.json",
+    ROOT_PATH / "webapp" / "pnpm-lock.yaml",
+    ROOT_PATH / "webapp" / "pnpm-workspace.yaml",
+    ROOT_PATH / "webapp" / "tsconfig.json",
+    ROOT_PATH / "widgets",
+    ROOT_PATH / "zztools" / "utils" / "__pycache__",
+    ROOT_PATH / ".editorconfig",
+    ROOT_PATH / ".gitattributes",
+    ROOT_PATH / ".gitignore",
+    ROOT_PATH / ".taurignore",
+    ROOT_PATH / "Cargo.lock",
+    ROOT_PATH / "Cargo.toml",
+    ROOT_PATH / "LICENSE",
+    ROOT_PATH / "README.md",
+    ROOT_PATH / "run.sh",
+    ROOT_PATH / "rust-toolchain.toml",
+    ROOT_PATH / "tauri.conf.json"
+}
+
+# ============================================================================ #
+
+def is_text_file(path: Path) -> bool:
     try:
-        with open(filepath, 'rb') as f:
-            chunk = f.read(1024)
-            if b'\0' in chunk:
-                return False
+        path.read_text(encoding="utf-8")
         return True
-    except Exception:
+    except UnicodeDecodeError:
         return False
 
-def check_copyright_year(file_path):
+# ============================================================================ #
+
+class LicenseHeaderChecker(Enum):
+    NO_ISSUES = 0
+    MISSING_HEADER = 1
+    MISSING_COPYRIGHT = 2
+    WRONG_COPYRIGHT_YEAR = 3
+    WRONG_COPYRIGHT_HOLDER = 4
+    MISSING_EPL_TEXT = 5
+    MISSING_SPDX = 6
+
+def check_epl_header(path: Path) -> LicenseHeaderChecker:
     try:
-        last_modified_year = datetime.fromtimestamp(os.path.getmtime(file_path), tz=timezone.utc).year
+        lines = path.read_text(encoding="utf-8", errors="replace").splitlines()
+        if lines[0].startswith("#!/usr/bin") or lines[0].startswith("#!/bin"):
+            lines = lines[1:]
 
-        with open(file_path, 'r', encoding='utf-8') as f:
-            content = f.read()
+        if len(lines) < 9:
+            return LicenseHeaderChecker.MISSING_HEADER
 
-            if "Copyright (c)" in content:
-                years = []
-                for match in re.findall(r"\b(\d{4})(?:\s*-\s*(\d{4}))?\b", content):
-                    start_year = int(match[0])
-                    end_year = int(match[1]) if match[1] else start_year
-                    years.append((start_year, end_year))
+        # Check second line if matches copyright pattern
+        match_copyright = re.search(r"Copyright \(c\) (\d{4}(?:-\d{4})?) \w+", lines[1])
+        if not match_copyright:
+            return LicenseHeaderChecker.MISSING_COPYRIGHT
+        else:
+            match_year = re.search(r"\d{4}(?:-\d{4})?", match_copyright.group(0))
+            copy_year = int(match_year.group(0).split("-")[-1])
+            current_year = datetime.now(timezone.utc).year
+            mtime_year = datetime.fromtimestamp(os.path.getmtime(path), timezone.utc).year
+            if (copy_year < mtime_year) or (copy_year > current_year):
+                return LicenseHeaderChecker.WRONG_COPYRIGHT_YEAR
 
-                if years:
-                    if not any(start_year <= last_modified_year <= end_year for start_year, end_year in years):
-                        logger.warning(f"Incorrect or missing copyright year in: {file_path}")
-    except Exception as e:
-        logger.error(f"Error checking copyright year in file {file_path}: {e}")
+        # Check fourth line if matches EPL text
+        if not re.search(r"This program and the accompanying materials are made", lines[3]):
+            return LicenseHeaderChecker.MISSING_EPL_TEXT
+
+        # Check fifth line if matches EPL text
+        if not re.search(r"available under the terms of the Eclipse Public License 2\.0", lines[4]):
+            return LicenseHeaderChecker.MISSING_EPL_TEXT
+
+        # Check sixth line if matches SPDX pattern
+        if not re.search(r"which is available at https://www\.eclipse\.org/legal/epl-2\.0/", lines[5]):
+            return LicenseHeaderChecker.MISSING_EPL_TEXT
+
+        # Check eighth line if matches SPDX pattern
+        if not re.search(r"SPDX-License-Identifier: EPL-2\.0", lines[7]):
+            return LicenseHeaderChecker.MISSING_SPDX
+
+        return LicenseHeaderChecker.NO_ISSUES
+    except OSError:
+        return None
 
 def main():
     logger.info("Checking license headers...")
-    ignore_dirs = [
-        ".forgejo",
-        ".git",
-        ".vscode",
-        "capabilities",
-        "coverage",
-        "dist",
-        "fontawesome",
-        "gen",
-        "node_modules",
-        "plugins",
-        "target",
-        "widgets",
-        "zztools"
-    ]
-    ignore_files = [
-        ".editorconfig",
-        ".eslintignore",
-        ".eslintrc*",
-        ".gitattributes",
-        ".gitignore",
-        ".npmrc",
-        ".nvmrc",
-        ".prettierrc*",
-        ".taurignore",
-        "Cargo.lock",
-        "Cargo.toml",
-        "LICENSE",
-        "package.json",
-        "pnpm-lock.yaml",
-        "pnpm-workspace.yaml",
-        "README.md",
-        "run.sh",
-        "rust-toolchain.toml",
-        "tauri.conf.json",
-        "tsconfig.json"
-    ]
 
-    for root, dirs, files in os.walk(ROOT_PATH):
-        dirs[:] = [d for d in dirs if d not in ignore_dirs]
+    missing = []
+    for file in sorted(ROOT_PATH.rglob("*")):
+        if not file.is_file():
+            continue
+        if any(file.is_relative_to(d) for d in EXCLUDE_PATHS):
+            continue
+        if not is_text_file(file):
+            continue
 
-        for file in files:
-            if any(fnmatch.fnmatch(file, pattern) for pattern in ignore_files):
-                continue
+        logger.debug(f"Checking {file}...")
+        header_status = check_epl_header(file)
+        if header_status == None:
+            missing.append(["UNREADABLE", file])
+        elif header_status != LicenseHeaderChecker.NO_ISSUES:
+            missing.append([header_status.name, file])
 
-            file_path = os.path.join(root, file)
-            try:
-                if not is_text_file(file_path):
-                    continue
+    if not missing:
+        logger.info("License header check completed.")
+        sys.exit(0)
 
-                with open(file_path, 'r', encoding='utf-8') as f:
-                    content = f.read()
-                    if "Eclipse Public License 2.0" not in content:
-                        logger.warning(f"Missing license header: {file_path}")
-
-                check_copyright_year(file_path)
-            except Exception as e:
-                logger.error(f"Error reading file {file_path}: {e}")
-
-    logger.info("License header check completed.")
+    logger.warn(f"Found {len(missing)} file(s) with license header issues:")
+    for [issue, path] in missing:
+        logger.warn(f" - [{issue}] ./{path.relative_to(ROOT_PATH)}")
+    sys.exit(1)
 
 if __name__ == "__main__":
     try:
