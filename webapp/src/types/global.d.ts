a90bc6930080624f799fab06e0b2959a49195ce7
Voguh <voguhofc@protonmail.com>
2026-05-21 14:32:01 -0300

    refactor(webapp): fix global variables definition

--- webapp/src/types/global.d.ts
+++ webapp/src/types/global.d.ts
@@ -7,65 +7,35 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  ******************************************************************************/
+/* eslint-disable no-var */
 
 declare global {
-    const __TAURI__: typeof import("@tauri-apps/api");
-    const __TAURI_PLUGIN_DIALOG__: typeof import("@tauri-apps/plugin-dialog");
-    const __TAURI_PLUGIN_LOG__: typeof import("@tauri-apps/plugin-log");
-    const __TAURI_PLUGIN_OPENER__: typeof import("@tauri-apps/plugin-opener");
-    const __TAURI_PLUGIN_OS__: typeof import("@tauri-apps/plugin-os");
-    const __TAURI_PLUGIN_STORE__: typeof import("@tauri-apps/plugin-store");
+    var __TAURI__: typeof import("@tauri-apps/api");
+    var __TAURI_PLUGIN_DIALOG__: typeof import("@tauri-apps/plugin-dialog");
+    var __TAURI_PLUGIN_LOG__: typeof import("@tauri-apps/plugin-log");
+    var __TAURI_PLUGIN_OPENER__: typeof import("@tauri-apps/plugin-opener");
+    var __TAURI_PLUGIN_STORE__: typeof import("@tauri-apps/plugin-store");
 
     /* ====================================================================== */
 
-    const __IS_DEV__: boolean;
-    const __PLATFORM__: string;
-    const UNICHAT_DISPLAY_NAME: string;
-    const UNICHAT_NAME: string;
-    const UNICHAT_VERSION: string;
-    const UNICHAT_DESCRIPTION: string;
-    const UNICHAT_AUTHORS: string;
-    const UNICHAT_HOMEPAGE: string;
-    const UNICHAT_ICON: string;
-    const UNICHAT_LICENSE_CODE: string;
-    const UNICHAT_LICENSE_NAME: string;
-    const UNICHAT_LICENSE_URL: string;
+    var __IS_DEV__: boolean;
+    var __PLATFORM__: "linux" | "macos" | "windows"; // Actually represents `std::env::consts::OS` from Rust (https://doc.rust-lang.org/std/env/consts/constant.OS.html)
 
-    const UNICHAT_GALLERY_DIR: string;
-    const UNICHAT_LICENSE_FILE: string;
-    const UNICHAT_PLUGINS_DIR: string;
-    const UNICHAT_WIDGETS_DIR: string;
+    var UNICHAT_DISPLAY_NAME: string;
+    var UNICHAT_NAME: string;
+    var UNICHAT_VERSION: string;
+    var UNICHAT_DESCRIPTION: string;
+    var UNICHAT_AUTHORS: string;
+    var UNICHAT_HOMEPAGE: string;
+    var UNICHAT_ICON: string;
+    var UNICHAT_LICENSE_CODE: string;
+    var UNICHAT_LICENSE_NAME: string;
+    var UNICHAT_LICENSE_URL: string;
 
-    /* ====================================================================== */
-
-    interface Window {
-        __TAURI__: typeof import("@tauri-apps/api");
-        __TAURI_PLUGIN_DIALOG__: typeof import("@tauri-apps/plugin-dialog");
-        __TAURI_PLUGIN_LOG__: typeof import("@tauri-apps/plugin-log");
-        __TAURI_PLUGIN_OPENER__: typeof import("@tauri-apps/plugin-opener");
-        __TAURI_PLUGIN_OS__: typeof import("@tauri-apps/plugin-os");
-        __TAURI_PLUGIN_STORE__: typeof import("@tauri-apps/plugin-store");
-
-        /* ================================================================== */
-
-        __IS_DEV__: boolean;
-        __PLATFORM__: string;
-        UNICHAT_DISPLAY_NAME: string;
-        UNICHAT_NAME: string;
-        UNICHAT_VERSION: string;
-        UNICHAT_DESCRIPTION: string;
-        UNICHAT_AUTHORS: string;
-        UNICHAT_HOMEPAGE: string;
-        UNICHAT_APP_ICON: string;
-        UNICHAT_LICENSE_CODE: string;
-        UNICHAT_LICENSE_NAME: string;
-        UNICHAT_LICENSE_URL: string;
-
-        UNICHAT_GALLERY_DIR: string;
-        UNICHAT_LICENSE_FILE: string;
-        UNICHAT_PLUGINS_DIR: string;
-        UNICHAT_WIDGETS_DIR: string;
-    }
+    var UNICHAT_GALLERY_DIR: string;
+    var UNICHAT_LICENSE_FILE: string;
+    var UNICHAT_PLUGINS_DIR: string;
+    var UNICHAT_WIDGETS_DIR: string;
 }
 
 export {};
